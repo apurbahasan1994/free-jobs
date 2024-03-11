3a75dc54da0dbc4cec0050fa7671849690d17d7a
Apurba Hasan <apurbahasan1994@gmail.com>
2024-03-12 01:14:58 +0600

    added constant

---
+++ backend/Utility/Utility.js
@@ -0,0 +1,24 @@
+export const JOB_STATUS = Object.freeze({
+  PENDING: "pending",
+  INTERVIEW: "interview",
+  DECLINED: "declined",
+});
+export const JOB_TYPE = Object.freeze({
+  FULL_IME: "full-time",
+  PART_TIME: "part-time",
+  INTERNSHIP: "internship",
+});
+
+export const JOB_SORT_BY = Object.freeze({
+  NEWEST_FIRST: "newest",
+  OLDEST_FIRST: "oldest",
+  ASC: "asc",
+  DESC: "desc",
+});
+
+export const USER_ROLES = Object.freeze({
+  SUPPER_ADMIN: "superuser",
+  CANDIDATE: "candidate",
+  COMPANY: "company",
+  USER: "user",
+});
